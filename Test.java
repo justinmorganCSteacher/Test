a92b44212ed9fcad6ba1997c4c8e0031d58b7629
Justin Morgan <justinmorganCSteacher@gmail.com>
2024-06-11 23:57:38 -0500

    Concatenate the x variable

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 public class Test {
     public static void main(String[] args) {
         int x = 5;
-        System.out.println("Hello World");
+        System.out.println("Hello World" + x);
     }
 }
