40ecfa2e584dab174e0193aad3bf9651703a6d39
Justin Morgan <justinmorganCSteacher@gmail.com>
2024-06-24 11:54:55 -0500

    changed to use for Leetcode challenge 55. canJump

--- Test.java
+++ Test.java
@@ -1,6 +1,30 @@
 public class Test {
     public static void main(String[] args) {
-        int x = 5;
-        System.out.println("Hello World");
+        System.out.println(new Test().canJump(new int[]{2,5,0,0}));
     }
+
+
+        public boolean canJump(int[] nums) {
+            int i = 0;
+            int j = 0;
+            while (i < nums.length - 1) {
+                if (nums[i] == 0) {
+                    boolean flag = false;
+                    for (int backup = i - 1; backup > j; j--) {
+                        if (nums[backup] != 0) {
+                            j = backup;
+                            i = backup + nums[backup];
+                            flag = true;
+                            break;
+                        }
+                    }
+                    if (!flag)
+                    return false;
+                } else {
+                j = i;
+                i += nums[i];
+                }
+            }
+            return true;
+        }
 }
